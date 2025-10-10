7ff2612a93b0084b86f8feef01e3ad73f8d261a0
NastassjaRosa <nastassja.paulhiac@gmail.com>
2025-10-10 07:06:47 +0200

    chore: initial Spring Boot skeleton (secure config, YAML, env vars)

---
+++ src/test/java/com/github/nastassjarosa/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nastassjarosa.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
